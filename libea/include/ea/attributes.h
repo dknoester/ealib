09466c53bebf757e53e279754a9ead107450f5cc
Dave Knoester <dave.knoester@gmail.com>
2012-06-08 12:19:00 -0400

    Draft #2 of novelty search.
    NS is now broken out as a separate top-level EA.  This makes sense, as
    it is fundamentally different than traditional EAs.

---
+++ libea/include/ea/attributes.h
@@ -0,0 +1,14 @@
+//
+//  attributes.h
+//  ealib
+//
+//  Created by Dave Knoester on 6/8/12.
+//  Copyright 2012 Michigan State University. All rights reserved.
+//
+
+#ifndef ealib_attributes_h
+#define ealib_attributes_h
+
+
+
+#endif
---
+++ libea/include/ea/comparators.h
@@ -0,0 +1,14 @@
+//
+//  comparators.h
+//  ealib
+//
+//  Created by Dave Knoester on 6/7/12.
+//  Copyright 2012 Michigan State University. All rights reserved.
+//
+
+#ifndef ealib_comparators_h
+#define ealib_comparators_h
+
+
+
+#endif
