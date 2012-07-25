787873ef1cae53e2e631d1cea58e8eab92cde09a
Dave Knoester <dave.knoester@gmail.com>
2012-07-25 11:53:53 -0400

    ealife & learning bot updates.

---
+++ libea/include/ea/artificial_life/spatial.h
@@ -0,0 +1,14 @@
+//
+//  spatial.h
+//  ealib
+//
+//  Created by Dave Knoester on 7/23/12.
+//  Copyright 2012 Michigan State University. All rights reserved.
+//
+
+#ifndef ealib_spatial_h
+#define ealib_spatial_h
+
+
+
+#endif
---
+++ libea/include/ea/selection.h
@@ -0,0 +1,14 @@
+//
+//  selection.h
+//  ealib
+//
+//  Created by Dave Knoester on 7/24/12.
+//  Copyright 2012 Michigan State University. All rights reserved.
+//
+
+#ifndef ealib_selection_h
+#define ealib_selection_h
+
+
+
+#endif
